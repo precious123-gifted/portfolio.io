9c29d1c99af7028ce016aedbcf48d3abdff2760f
precious123-gifted <58713062+precious123-gifted@users.noreply.github.com>
2022-05-13 17:40:09 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const landscapeMediaQuery = window.matchMedia('(orientation:landscape)')
 const Contact = document.querySelector('#contact')
+const menu = document.querySelector('.contactMenu')
+const exitIcon = document.querySelector('.exit')
 
+const slideMenuIn = ()=>{
+    menu.style.animation = 'slidein 1.01s ease'
+    setTimeout(()=>{ menu.style.left = '77%'},1000)
+}
 
+const slideMenuOut = ()=>{
+    menu.style.animation = 'slideout 1.01s ease'
+    setTimeout(()=>{ menu.style.left = '100%'},1000)
+}
 
 // Check if the media query is true
 if (landscapeMediaQuery.matches) {
   Contact.addEventListener('click',()=>{
-      alert('working')
-  })
+      slideMenuIn()
+   })
+
+   exitIcon.addEventListener('click',()=>{
+       slideMenuOut()
+   })
 }
